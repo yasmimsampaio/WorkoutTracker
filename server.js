8b99958efa916991503946b21dd25af78a8997ba
Yasmim Sampaio <yasmimsampaio10@hotmail.com>
2020-12-23 16:48:19 -0500

    deploying

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // routes
 app.use(require("./routes/api.js"));
