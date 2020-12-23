baf43ee167692bd81dd7541c631759461d6a589a
Yasmim Sampaio <yasmimsampaio10@hotmail.com>
2020-12-23 15:38:51 -0500

    fixed the add exercise

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,6 @@ const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
-      .sort({ date: -1 })
       .then(dbWorkout => {
         res.json(dbWorkout);
       })
@@ -12,20 +11,18 @@ router.get("/api/workouts", (req, res) => {
       });
   });
   
-router.put("/api/workout:/id", function(req, res) {
+router.put("/api/workouts/:id", function(req, res) {
     var condition = "id = " + req.params.id;
-  
-    workout.update({
-      exercise: req.body.exercise
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
+  console.log(condition);
+    Workout.findByIdAndUpdate(req.params.id,
+        {$push:{exercises:req.body}},)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
     });
-});
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.insertMany(body)
